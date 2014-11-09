c9e4e086ace2bb7d3d1c1139e5de9262b53c5ede
Lexxes <devLexxes@gmail.com>
2014-11-09 21:21:27 +0100

    chatfix

--- Ultimate Carry Prevolution/Ultimate Carry Prevolution/Chat.cs
+++ Ultimate Carry Prevolution/Ultimate Carry Prevolution/Chat.cs
@@ -11,6 +11,7 @@ namespace Ultimate_Carry_Prevolution
 				"<font color ='{0}'>Ultimate</font> <font color ='{1}'>Carry</font> <font color ='{1}'>Prevolution</font>",
 				HtmlColor.Cyan, HtmlColor.Cyan);
 			Game.PrintChat("<font color ='{0}'>By xSalice & Lexxes </font>", HtmlColor.Gold);
+
 			Game.PrintChat(
 				"<font color ='{0}'>Version </font> <font color ='{1}'>" + Loader.VersionNumber +
 				"</font> <font color ='{0}'>Loading</font>",
